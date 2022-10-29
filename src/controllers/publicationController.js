87e8dd7efa93834f2b80414701a309ddc78af17f
iezhkv <lev3el6up9@gmail.com>
2022-10-30 00:03:03 +0300

    render edit page

--- src/controllers/publicationController.js
+++ src/controllers/publicationController.js
@@ -3,6 +3,28 @@ const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware')
 const publicationService = require('../services/publicationService');
 
+router.get('/' , async (req, res) => {
+    const publications = await publicationService.getAll().lean();
+    res.render('publication', { publications });
+});
+
+router.get('/:publicationId/details' , async (req, res) => {
+    const publication = await publicationService.getOneDetailed(req.params.publicationId).lean();
+    const isAuthor = publication.author._id == req.user?._id;
+
+    res.render('publication/details', { ...publication, isAuthor });
+});
+
+router.get('/:publicationId/edit' ,isAuth, async (req, res, next) => {
+    const publication = await publicationService.getOne(req.params.publicationId);    
+    //IsAuthor
+    if (publication.author != req.user._id) {
+        return next({massage: 'You are not authorized', status: 401});
+    }
+
+    res.render('publication/edit', {...publication});
+});
+
 
 router.get('/create', isAuth, (req, res) =>{
     res.render('publication/create');
--- src/services/publicationService.js
+++ src/services/publicationService.js
@@ -6,3 +6,13 @@ exports.create = (publicationData) => {
     const createdPublication = Publication.create(publicationData);
 
 }
+exports.getAll = () => Publication.find();
+exports.getOne = (publicationId) => Publication.findById(publicationId);
+exports.getOneDetailed = (publicationId) => Publication.findById(publicationId).populate('author');
+exports.delete = (publicationId) => Publication.findByIdAndDelete(publicationId);
+exports.update = (publicationId, publicationData) => Publication.updateOne({id: publicationId}, {$set: publicationData});
+
+
+
+
+     
